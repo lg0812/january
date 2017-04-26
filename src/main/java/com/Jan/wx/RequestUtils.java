d9b0669b3d7e0bf9c7702f4b7bdf3c78690daef1
hsx-lghh0812 <zhang.renwu@successinfo.com.cn>
2017-04-26 10:09:00 +0800

    modified:   src/main/java/com/Jan/wx/RequestUtils.java 	modified:   src/test/java/com/Jan/wx/Wechat4Test.java

--- src/main/java/com/Jan/wx/RequestUtils.java
+++ src/main/java/com/Jan/wx/RequestUtils.java
@@ -3,7 +3,10 @@ package com.Jan.wx;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -16,12 +19,50 @@ import org.apache.logging.log4j.Logger;
 public class RequestUtils {
 	final static Logger log = LogManager.getLogger(RequestUtils.class);
 
+	// url只包含？号前面的部分,hashMap 是参数
 	public static String getRequest(String url, Map<String, String> hashMap) {
 		BufferedReader bis;
 		StringBuffer str = new StringBuffer("");
 		try {
 			// 如果hashmap是null 或者长度为0 ,不加？
-			URLConnection http = new URL(
+			HttpURLConnection http = (HttpURLConnection) new URL(
+					url + ((hashMap == null || hashMap.isEmpty()) ? "" : "?") + getParamsFromMap(hashMap))
+							.openConnection();
+			log.info(http.getURL());
+			http.setRequestProperty("accept", "*/*");
+			http.setRequestProperty("connection", "Keep-Alive");
+			// 这是从浏览器中copy出来的
+			http.setRequestProperty("user-agent",
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36");
+			http.setRequestMethod("GET");
+			// 这里的时间单位是ms
+			http.setRequestProperty("Charset", "utf-8");
+			http.setConnectTimeout(30000);
+			http.setReadTimeout(30000);
+			http.connect();
+			bis = new BufferedReader(new InputStreamReader(http.getInputStream(), "utf-8"));
+			String line = "";
+			while ((line = bis.readLine()) != null) {
+				str.append(line);
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		log.info(str.toString());
+		return str.toString();
+	}
+
+	// url只包含？号前面的部分,hashMap 是参数,body是需要写到body中的参数的值
+	public static String postRequest(String url, Map<String, String> hashMap, String body) {
+		BufferedReader bis;
+		StringBuffer str = new StringBuffer("");
+		try {
+			// 如果hashmap是null 或者长度为0 ,不加？
+			HttpURLConnection http = (HttpURLConnection) new URL(
 					url + ((hashMap == null || hashMap.isEmpty()) ? "" : "?") + getParamsFromMap(hashMap))
 							.openConnection();
 			log.info(http.getURL());
@@ -31,7 +72,16 @@ public class RequestUtils {
 			http.setRequestProperty("user-agent",
 					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36");
 			http.setRequestProperty("Charset", "utf-8");
+			http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; encoding=utf-8");
+			http.setRequestMethod("POST");
+			http.setDoOutput(true);
+			http.setDoInput(true);
+			// 这里的时间单位是ms
+			http.setConnectTimeout(30000);
+			http.setReadTimeout(30000);
 			http.connect();
+			OutputStream os = http.getOutputStream();
+			os.write(body.getBytes());
 			bis = new BufferedReader(new InputStreamReader(http.getInputStream(), "utf-8"));
 			String line = "";
 			while ((line = bis.readLine()) != null) {
@@ -61,5 +111,4 @@ public class RequestUtils {
 		log.info("getRequest params:" + str);
 		return str;
 	}
-
 }
--- src/test/java/com/Jan/wx/Wechat4Test.java
+++ src/test/java/com/Jan/wx/Wechat4Test.java
@@ -6,10 +6,29 @@ import java.util.Map;
 import org.junit.Test;
 
 public class Wechat4Test {
+	public static String accessToken = "VMg4cPNtVfq5QeRMy-PIZ_a1C8GX2PMBukOctUTZKJvz-hveMjvzVhAJE7apToIvMjAjRNETXCiCsJgUdkcaUDBe8jvyFS31YHfBl2MPhNuVmsXKvbqUSIk9tMi7piQuHADfAHAOHK";
+
 	@Test
 	public void testGetReq() {
 		Map map = new HashMap<String, String>();
 		map.put("str", "this is a test case!");
-		System.out.println(RequestUtils.getRequest("http://127.0.0.1:8081/january/wx/test_success", map));
+		RequestUtils.getRequest("http://127.0.0.1:8081/january/wx/test_success", map);
+	}
+
+	// 创建菜单
+	@Test
+	public void testPostReq() {
+		String menu = "{\"button\" : [ {\"type\" : \"view\",\"name\" : \"我的订单\",\"url\" : \"http://ryy-api.xiaokejia.com/public/html/order_list.html\" }, {\"type\" : \"view\", \"name\" : \"我的积分\",\"url\" : \"http://wfx.xiaokejia.com/decorate/html/index.html\" }, { \"type\" : \"view\",\"name\" : \"我最喜欢\",\"url\" : \"http://wfx.xiaokejia.com/decorate/html/index.html\" } ]} ";
+		Map map = new HashMap<String, String>();
+		map.put("access_token", accessToken);
+		RequestUtils.postRequest("https://api.weixin.qq.com/cgi-bin/menu/create", map, menu);
+	}
+
+	// 获取菜单
+	@Test
+	public void testGetMenu() {
+		Map map = new HashMap<String, String>();
+		map.put("access_token", accessToken);
+		RequestUtils.getRequest("https://api.weixin.qq.com/cgi-bin/menu/delete", map);
 	}
 }
