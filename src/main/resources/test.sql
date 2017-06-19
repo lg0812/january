c782c20c856e17a60643619c6dd2abc87773f3b8
hsx-lghh0812 <zhang.renwu@successinfo.com.cn>
2017-06-19 20:25:22 +0800

    Update test.sql

--- src/main/resources/test.sql
+++ src/main/resources/test.sql
@@ -1,5 +1,10 @@
 use january;
 select * from usertoken;
 select * from User;
+select * from User;
+
+
+
+select * from User;
 select * from usertoken;
 update usertoken set accessToken = "abc" where accessTokenId = 1;
