f62d6dd90bf4cb401863a7e393c0977017ceb578
LG0812 <2356581512@qq.com>
2017-09-06 17:54:47 +0800

    all

--- src/main/webapp/mobile/js/public.js
+++ src/main/webapp/mobile/js/public.js
@@ -4,12 +4,28 @@
 
 function pro_bottom() {
     var temp = `<div class="fixed-bottom">
-            <div class="d-flex justify-content-lg-around tc">
-                <div style="flex-grow: 1">进入店铺</div>
-    			<div style="flex-grow: 2">收藏</div>
-                <div style="flex-grow: 2">加入购物车</div>
-                <div style="flex-grow: 2">立即购买</div>
+            <div class="d-flex justify-content-lg-around pro-footer">
+                <div style="flex-grow: 1;flex-shrink: 1;">
+                	<img src="images/home.png" style="width:1.5rem;height:1.5rem;padding: .25rem 0px 0px;"/>
+                	<div>店铺</div>
+                </div>
+                
+    			<div style="flex-grow: 1;flex-shrink: 1;">
+    				<img src="images/collection.png" style="width:1.5rem;height:1.5rem;padding: .25rem 0px 0px;"/>
+    				<div>收藏</div>
+    			</div>
+    			
+                <div style="flex-grow: 1;flex-shrink: 1;">
+    				<img src="images/cart.png" style="width:1.5rem;height:1.5rem;padding: .25rem 0px 0px;"/>
+    				<div>购物车</div>
+    			</div>
+    			
+                <div style="flex-grow: 2;flex-shrink: 2;background:#ff4f39;color:white;font-size:1rem;" class="d-flex justify-content-center align-items-center">
+                	加入购物车
+                </div>
+                
             </div>
 	    </div>`
     return temp;
 }
+
