d5df4e376053b053721b3ebb628af5910dadd088
LG0812 <235681512@qq.com>
2017-09-25 20:48:40 +0800

    none

--- src/test/java/com/Jan/mybatisTest/StartDemo.java
+++ src/test/java/com/Jan/mybatisTest/StartDemo.java
@@ -1,26 +1,22 @@
 package com.Jan.mybatisTest;
 
-import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.Jan.mappers.UserMapper;
 import com.Jan.model.User;
 import com.alibaba.fastjson.JSON;
 
 @RunWith(SpringRunner.class)
+@WebAppConfiguration
 @ContextConfiguration({ "classpath:spring-mvc.xml" })
-@Transactional
 public class StartDemo {
-
+	@Autowired
 	private UserMapper userMapper;
-
-	public void setUserMapper(UserMapper userMapper) {
-		this.userMapper = userMapper;
-	}
 
 	public User getUser(Long id) {
 		return this.userMapper.getUser(id);
