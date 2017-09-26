3aec822cdf9c8e3d140fa5a99f4494513cc3c74e
LG0812 <235681512@qq.com>
2017-09-26 08:11:07 +0800

    none

---
+++ src/main/java/com/Jan/mappers/GoodsInfoMapper.java
@@ -0,0 +1,7 @@
+package com.Jan.mappers;
+
+import com.Jan.may.GoodsInfo;
+
+public interface GoodsInfoMapper {
+	GoodsInfo getGoodsInfoById(Long id);
+}
--- src/test/java/com/Jan/mybatisTest/StartDemo.java
+++ src/test/java/com/Jan/mybatisTest/StartDemo.java
@@ -7,7 +7,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.Jan.mappers.GoodsInfoMapper;
 import com.Jan.mappers.UserMapper;
+import com.Jan.may.GoodsInfo;
 import com.Jan.model.User;
 import com.alibaba.fastjson.JSON;
 
@@ -18,13 +20,25 @@ public class StartDemo {
 	@Autowired
 	private UserMapper userMapper;
 
+	@Autowired
+	private GoodsInfoMapper goodsInfoMapper;
+
 	public User getUser(Long id) {
 		return this.userMapper.getUser(id);
 	}
 
-	@Test
+	public GoodsInfo getGoodsInfo(Long id) {
+		return this.goodsInfoMapper.getGoodsInfoById(id);
+	}
+
+	// @Test
 	public void ttt() {
 		System.out.println(JSON.toJSONString(getUser(1L)));
+	}
+
+	@Test
+	public void tttt() {
+		System.out.println(JSON.toJSONString(getGoodsInfo(1L)));
 	}
 
 }
